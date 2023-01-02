97772f1657d1905b9deac0c6fbe52007278606de
joeAmc <joe@limio.com>
2023-01-02 13:14:58 +0000

    header on small devices updated necklaces imgs added

--- src/components/Cards/necklaceData.js
+++ src/components/Cards/necklaceData.js
@@ -1,10 +1,22 @@
 import img2 from "../../assets/Logo.png";
+import sun1 from "../../assets/necklaces/necklace1.jpg";
+import sun2 from "../../assets/necklaces/necklace2.jpg";
+import hand1 from "../../assets/necklaces/necklace3.jpg";
+import hand2 from "../../assets/necklaces/necklace4.jpg";
 
 export const necklaces = [
   {
     id: Math.random(),
-    image: [img2, img2, img2],
-    title: " Triangular stackable ring",
+    image: [sun1, sun2],
+    title: "Sunrise",
+    material: "gold filled",
+    price: 35,
+    url: "https://www.etsy.com/uk/listing/1246783193/gold-filled-ring-triangular-stackable?click_key=e0b71b463e8be4ff37724f03a7b33c1cd8e7c530%3A1246783193&click_sum=1d27a000&ref=shop_home_active_6&frs=1",
+  },
+  {
+    id: Math.random(),
+    image: [hand1, hand2],
+    title: "Sunrise",
     material: "gold filled",
     price: 35,
     url: "https://www.etsy.com/uk/listing/1246783193/gold-filled-ring-triangular-stackable?click_key=e0b71b463e8be4ff37724f03a7b33c1cd8e7c530%3A1246783193&click_sum=1d27a000&ref=shop_home_active_6&frs=1",
